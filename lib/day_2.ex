d41132542da39c9c05ef8f57519f254382a5786b
Jamin Thornsberry <jthornsberry@aspirehealthcare.com>
2017-12-04 23:46:49 -0600

    Better docs

--- lib/day_2.ex
+++ lib/day_2.ex
@@ -11,39 +11,41 @@ defmodule Day2 do
 
   The spreadsheet consists of rows of apparently-random numbers. To make sure
   the recovery process is on the right track, they need you to calculate the
-  spreadsheet's checksum. For each row, determine the difference between the
+  spreadsheet's _checksum_. For each row, determine the difference between the
   largest value and the smallest value; the checksum is the sum of all of these
   differences.
 
   For example, given the following spreadsheet:
 
-      5 1 9 5
-      7 5 3
-      2 4 6 8
+  ```
+  5 1 9 5
+  7 5 3
+  2 4 6 8
+  ```
 
-  - The first row's largest and smallest values are 9 and 1, and their
-    difference is 8.
-  - The second row's largest and smallest values are 7 and 3, and their
-    difference is 4.
-  - The third row's difference is 6.
+  * The first row's largest and smallest values are `9` and `1`, and their
+    difference is `8`.
+  * The second row's largest and smallest values are `7` and `3`, and their
+    difference is `4`.
+  * The third row's difference is `6`.
 
-  In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+  In this example, the spreadsheet's checksum would be `8 + 4 + 6 = 18`.
 
     iex> Day2.solve_part_1("5 1 9 5
     ...> 7 5 3
     ...> 2 4 6 8")
     18
 
-  What is the checksum for the spreadsheet in your puzzle input?
+  _What is the checksum_ for the spreadsheet in your puzzle input?
 
   --- Part Two ---
 
-  "Great work; looks like we're on the right track after all. Here's a star for
-  your effort." However, the program seems a little worried. Can programs be
-  worried?
+  "Great work; looks like we're on the right track after all. Here's a _star_
+  for your effort." However, the program seems a little worried. Can programs
+  _be_ worried?
 
   "Based on what we're seeing, it looks like all the User wanted is some
-  information about the evenly divisible values in the spreadsheet.
+  information about the _evenly divisible values_ in the spreadsheet.
   Unfortunately, none of us are equipped for that kind of calculation - most of
   us specialize in bitwise operations."
 
@@ -54,23 +56,25 @@ defmodule Day2 do
 
   For example, given the following spreadsheet:
 
-      5 9 2 8
-      9 4 7 3
-      3 8 6 5
+  ```
+  5 9 2 8
+  9 4 7 3
+  3 8 6 5
+  ```
 
-  - In the first row, the only two numbers that evenly divide are 8 and 2; the
-    result of this division is 4.
-  - In the second row, the two numbers are 9 and 3; the result is 3.
-  - In the third row, the result is 2.
+  * In the first row, the only two numbers that evenly divide are `8` and `2`;
+    the result of this division is `4`.
+  * In the second row, the two numbers are `9` and `3`; the result is `3`.
+  * In the third row, the result is `2`.
 
-  In this example, the sum of the results would be 4 + 3 + 2 = 9.
+  In this example, the sum of the results would be `4 + 3 + 2 = 9`.
 
     iex> Day2.solve_part_2("5 9 2 8
     ...> 9 4 7 3
     ...> 3 8 6 5")
     9
 
-  What is the sum of each row's result in your puzzle input?
+  What is the _sum of each row's result_ in your puzzle input?
   """
 
   @doc """
