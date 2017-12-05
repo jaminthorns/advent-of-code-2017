8effc7ae646bbe8f3ea3791cf3e4c4230a9f628b
Jamin Thornsberry <jthornsberry@aspirehealthcare.com>
2017-12-04 22:40:24 -0600

    Solve Day 3 non-mathematically

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -60,31 +60,31 @@ defmodule Day3 do
   Square 1 starts with the value 1.
 
     iex> Day3.solve_part_2("1")
-    1
+    2
 
   Square 2 has only one adjacent filled square (with value 1), so it also stores
   1.
 
     iex> Day3.solve_part_2("2")
-    1
+    4
 
   Square 3 has both of the above squares as neighbors and stores the sum of
   their values, 2.
 
     iex> Day3.solve_part_2("3")
-    2
+    4
 
   Square 4 has all three of the aforementioned squares as neighbors and stores
   the sum of their values, 4.
 
     iex> Day3.solve_part_2("4")
-    4
+    5
 
   Square 5 only has the first and fourth squares as neighbors, so it gets the
   value 5.
 
     iex> Day3.solve_part_2("5")
-    5
+    10
 
   Once a square is written, its value does not change. Therefore, the first few
   squares would receive the following values:
@@ -102,33 +102,70 @@ defmodule Day3 do
   Solve part 1 of day 3's puzzle.
   """
   def solve_part_1(input) do
-    input
-    |> String.to_integer
-    |> calculate_distance
+    n = String.to_integer(input)
+
+    generate_grid(0, &get_distance/2)
+    |> Enum.at(n - 1)
   end
 
   @doc """
   Solve part 2 of day 3's puzzle.
   """
-  def solve_part_2(_input) do
-    :not_implemented
+  def solve_part_2(input) do
+    n = String.to_integer(input)
+
+    generate_grid(1, &sum_surrounding/2)
+    |> Stream.filter(& &1 > n)
+    |> Enum.at(0)
   end
 
-  defp calculate_distance(1), do: 0
-  defp calculate_distance(n) do
-    index = n - 1
-    offset = calculate_offset(index)
-
-    triangle(index, offset) + offset
+  defp generate_grid(initial_value, next) do
+    initial_value
+    |> get_initial_grid
+    |> Stream.iterate(&get_next_grid(&1, next))
+    |> Stream.map(fn {grid, coordinate, _} -> Map.get(grid, coordinate) end)
   end
 
-  defp calculate_offset(x) do
-    (:math.sqrt(x) - 1) / 2
-    |> Float.ceil
-    |> trunc
+  defp get_initial_grid(initial_value) do
+    {%{{0, 0} => initial_value}, {0, 0}, :south}
   end
 
-  defp triangle(x, height) do
-    abs(rem(x, height * 2) - height)
+  defp get_next_grid({grid, coordinate, direction}, next) do
+    {next_coordinate, next_direction} = move_forward(grid, coordinate, direction)
+    next_grid = Map.put(grid, next_coordinate, next.(grid, next_coordinate))
+
+    {next_grid, next_coordinate, next_direction}
+  end
+
+  defp move_forward(grid, coordinate, direction) do
+    forward = move(coordinate, direction)
+    turned = move(coordinate, turn(direction))
+
+    if Map.has_key?(grid, turned),
+      do: {forward, direction},
+      else: {turned, turn(direction)}
+  end
+
+  defp move({x, y}, :north), do: {x, y + 1}
+  defp move({x, y}, :west), do: {x - 1, y}
+  defp move({x, y}, :south), do: {x, y - 1}
+  defp move({x, y}, :east), do: {x + 1, y}
+
+  defp turn(:north), do: :west
+  defp turn(:west), do: :south
+  defp turn(:south), do: :east
+  defp turn(:east), do: :north
+
+  defp get_distance(_grid, {x, y}) do
+    abs(x) + abs(y)
+  end
+
+  def sum_surrounding(grid, {x, y}) do
+    for offset_x <- -1..1,
+        offset_y <- -1..1,
+        offset_x != 0 or offset_y != 0 do
+      Map.get(grid, {x + offset_x, y + offset_y}, 0)
+    end
+    |> Enum.sum
   end
 end
